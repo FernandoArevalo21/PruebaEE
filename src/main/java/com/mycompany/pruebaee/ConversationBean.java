ef2d60c5049ac1bc215b257c67c9cb3713729009
FernandoArevalo21 <fernandoarevalo2000@hotmail.com>
2020-10-24 19:12:23 -0500

    Se creo el codigo ejemplo completo

---
+++ src/main/java/com/mycompany/pruebaee/ConversationBean.java
@@ -0,0 +1,79 @@
+/*
+ * Clase encargada de contener la parte logica del codigo
+ */
+package com.mycompany.pruebaee;
+
+import javax.inject.Named;
+import javax.enterprise.context.ConversationScoped;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Conversation;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Johan Andres Sanchez
+ * @author Fernando Arévalo
+ */
+@Named(value = "conversationBean")
+@ConversationScoped
+public class ConversationBean implements Serializable {
+  
+  @Inject
+  private Conversation conversation;
+  
+  private int counter;
+  
+  // El codigo solo se inicializa una vez
+  @PostConstruct
+  public void init(){
+    counter = 0;
+  }
+  /*
+   * Esta función inicia la conversación
+  */
+  public void initConversation(){
+    if (!FacesContext.getCurrentInstance().isPostback() 
+      && conversation.isTransient()) {
+      
+      conversation.begin();
+    }
+  }
+  /*
+  * Se crea el metodo por el cual se ve el auto incremento de los números
+  */
+  public void increment(){
+    counter++;
+  }
+  /*
+   *Metodo con el cual se logra hacer el cambio de pagina 
+  */
+  public String handleFirstStepSubmit(){
+    return "step2?faces-redirect=true";
+  }
+  /*
+   *Metodo con el cual se logra terminar todo el programa
+  */
+  public String endConversation(){
+    if(!conversation.isTransient()){
+      conversation.end();
+    }
+    return "step1?faces-redirect=true";
+  }
+
+  public int getCounter() {
+    return counter;
+  }
+
+  public void setCounter(int counter) {
+    this.counter = counter;
+  }
+
+  public Conversation getConversation() {
+    return conversation;
+  }
+    public ConversationBean() {
+    }
+    
+}
